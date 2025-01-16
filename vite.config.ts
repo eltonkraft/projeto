461212c573232f6906f5ca829d413776be1f76d4
eltonkraft <eltonlothar@gmail.com>
2025-01-16 12:56:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/projeto/", // Ajuste a base para o subdiretório do seu repositório no GitHub
+});
